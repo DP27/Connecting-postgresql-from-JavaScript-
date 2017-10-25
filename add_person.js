512f0a0e39e2241116ba6cbd650fb012c7adf404
Dishant <dishantpatel27@gmail.com>
2017-10-25 03:09:22 +0000

    created new migration by adding famous_people_id

--- add_person.js
+++ add_person.js
@@ -9,5 +9,6 @@ const args = process.argv.slice(2);
 console.log(args);
 
 knex('famous_people').insert({first_name : args[0],last_name : args[1],birthdate : args[2]}).then(results => console.log(results)).catch(err => console.log(err));
+knex.destroy();
 
 
---
+++ migrations/20171024230840_add_famous_person_id_to_milestones.js
@@ -0,0 +1,34 @@
+const pg = require("pg");
+const settings = require("../settings"); // settings.json
+const args = process.argv.slice(2);
+const knex  = require('knex')({
+  client : 'pg',
+  connection :{
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+
+  }
+
+});
+
+
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones',function(table){
+      table.integer('famous_person_id').unsigned();
+      table.foreign('famous_person_id').references('famous_people.id');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropColumn('famous_person_id')
+    ])
+
+};
